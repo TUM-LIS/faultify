bd54b3f3988aa6d0f87b9c8686668d3127b6adde
David May <david.may.muc@googlemail.com>
2014-11-24 15:22:26 +0100

    fp fault injection functions

--- analysis/metrics/bitmanipulation.h
+++ analysis/metrics/bitmanipulation.h
@@ -1,2 +1,5 @@
 int byte_to_bit(int endian, unsigned char *ibuf, int ilen, unsigned char *obuf, int *olen);
 int bit_to_byte(int endian, unsigned char *ibuf, int ilen, unsigned char *obuf, int *olen);
+int fp_precision_reduction(double *in,unsigned int len,unsigned int precision);
+int fp_uniform_fault_injection(double *in,unsigned int len, double p_e);
+int fp_fault_injection(double *in,unsigned int len, double *p_e);
