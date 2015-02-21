89769ee82bd12c7d3c4691f0c42a972b4fc94ddb
David May <david.may.muc@googlemail.com>
2015-02-21 16:15:28 +0100

    fix

--- analysis/optimization/matlab/viterbi/plotData.py
+++ analysis/optimization/matlab/viterbi/plotData.py
@@ -297,16 +297,35 @@ approx = [db0, db2, db4, db6, db8, db10, db12, db14, db16, db18, db20, db22, db2
 
 import matplotlib.pyplot as plt
 import numpy as np
+import pylab
+from pylab import arange,pi,sin,cos,sqrt
+
+
+fig_width_pt = 253.04987 # Get this from LaTeX using \showthe\columnwidth
+inches_per_pt = 1.0/72.27               # Convert pt to inches
+golden_mean = (sqrt(5)-1.0)/2.0         # Aesthetic ratio
+fig_width = fig_width_pt*inches_per_pt  # width in inches
+fig_height =fig_width*golden_mean       # height in inches
+fig_size = [fig_width,fig_height]
+params = {'backend': 'ps',
+          'axes.labelsize': 8,
+          'text.fontsize': 8,
+          'legend.fontsize': 8,
+          'xtick.labelsize': 8,
+          'ytick.labelsize': 8,
+          'text.usetex': True,
+          'figure.figsize': fig_size}
+
+pylab.rcParams.update(params)
 
 t = np.arange(0, 52, 2)
 #print t
-
-plt.figure()
-plt.plot(t,approx,'r')
-plt.xlabel('Signal-to-Noise Ratio [dB]')
-plt.ylabel(r'Tolerated sum of $p_e$')
-plt.show()
-
-
-
+pylab.figure(1)
+pylab.clf()
+pylab.axes([0.18,0.17,0.95-0.17,0.95-0.22])
+pylab.plot(t,approx,'r')
+pylab.xlabel('Signal-to-Noise Ratio [dB]')
+pylab.ylabel(r'Tolerated sum of $p_e$')
+pylab.savefig('optimization_viterbi.pdf')
+#pylab.show()
 
